5487817e0a9da44c96ff5327cad829e479541762
Isaul <tiradolisaul@gmail.com>
2024-10-21 18:57:44 -0700

    emnums created

--- editor.py
+++ editor.py
@@ -1,6 +1,34 @@
+from enum import Enum
+
+
 def main():
     pass
 
 
 if __name__ == '__main__':
     main()
+
+
+class Formatter(Enum):
+    PLAIN = 'plain'
+    BOLD = 'bold'
+    ITALIC = 'italic'
+    HEADER = 'header'
+    LINK = 'link'
+    INLINE = 'inline-code'
+    ORDERED_LIST = 'ordered-list'
+    UNORDERED_LIST = 'unordered-list'
+    NEW_LINE = 'new-line'
+
+
+class Features(Enum):
+    HELP = '!help!'
+    DONE = '!done'
+
+
+def prompt_formater() -> str:
+    return input("Choose a formatter: ")
+
+def run():
+    pass
+
